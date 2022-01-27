8c16e954abab13eba48bbd6efd8fad54c9c6df1b
Julien Viet <julien@julienviet.com>
2022-01-27 15:10:00 +0100

    Documentation and examples.

--- src/main/java/examples/UriTemplateExamples.java
+++ src/main/java/examples/UriTemplateExamples.java
@@ -1,11 +1,26 @@
 package examples;
 
+import io.vertx.docgen.Source;
 import io.vertx.uritemplate.UriTemplate;
 import io.vertx.uritemplate.Variables;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+@Source
 public class UriTemplateExamples {
 
-  public void simpleExample() {
+  public void exampleCreate() {
+    UriTemplate template = UriTemplate.of("http://{host}/product/{id}{?sort}");
+  }
+
+  public void exampleInvalid() {
+    UriTemplate template = UriTemplate.of("{!invalid}}"); // Will throw an exception
+  }
+
+  public void exampleExpansion() {
     UriTemplate template = UriTemplate.of("http://{host}/product/{id}{?sort}");
     String uri = template.expandToString(Variables
       .variables()
@@ -13,5 +28,21 @@ public class UriTemplateExamples {
       .set("id", "12345")
       .set("sort", "price")
     );
+  }
+
+  public void exampleSingleValueVariable(Variables variables) {
+    variables.set("server", "localhost");
+    variables.set("port", "8080");
+  }
+
+  public void exampleListVariable(Variables variables) {
+    variables.set("ids", Arrays.asList("123", "456"));
+  }
+
+  public void exampleMapVariable(Variables variables) {
+    Map<String, String> query = new HashMap<>();
+    query.put("firstName", "Dale");
+    query.put("lastName", "Cooper");
+    variables.set("query", query);
   }
 }
